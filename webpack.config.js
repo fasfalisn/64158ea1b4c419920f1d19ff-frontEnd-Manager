6b2956a54930429c4e87b4c3a1594866be395e3c
Nikiforos Fasfalis <fasfalis.n@gmail.com>
2023-07-11 15:08:19 +0300

    My commit message

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+	'Home' : './javascript/Home.js',
+	'addEmployee' : './javascript/addEmployee.js',
+	'addTask' : './javascript/addTask.js',
+	'updateEmployee' : './javascript/updateEmployee.js',
+	'updateTask' : './javascript/updateTask.js',
+},
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+    
+  },
+};
